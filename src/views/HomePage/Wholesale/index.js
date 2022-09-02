f7ff05212a8757a2731de1813a1bb7a4400b532b
brendino500 <brenda.ty@live.com>
2022-09-02 16:03:25 +0100

    fix: Update button styling

--- src/views/HomePage/Wholesale/index.js
+++ src/views/HomePage/Wholesale/index.js
@@ -31,9 +31,7 @@ function WholesaleSection() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <Button className="" secondary>
-            shop
-          </Button>
+          <Button secondary>shop</Button>
         </div>
       </div>
     </SectionWrapper>
