af765b3c413891374a5ee614e52996d36c6b98b1
brendino500 <brenda.ty@live.com>
2022-09-02 15:15:52 +0100

    feat: Add dummy nav

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,30 @@
+import Image from 'next/image'
+import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
+
 function Menu() {
-  return <h1>This is a Menu</h1>
+  const menuItems = ['Home', 'Shop', 'About', 'Visit', 'Learn', 'Wholesale']
+  return (
+    <nav className="flex align-center justify-between">
+      <div>
+        <Image />
+      </div>
+      <ul className="flex flex-row align-center font-title justify-center m-3">
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Learn</li>
+        <li>Wholesale</li>
+      </ul>
+      <div className="flex align-center m-3">
+        <button>
+          <IoSearchOutline />
+        </button>
+        <button>
+          <IoCartOutline />
+        </button>
+      </div>
+    </nav>
+  )
 }
 
 export default Menu
