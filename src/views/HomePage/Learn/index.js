4c85a3769bce13678a0710cd540a7207b9cc4946
brendino500 <brenda.ty@live.com>
2022-09-04 21:55:15 +0100

    fix: Add data for learn section

--- src/views/HomePage/Learn/index.js
+++ src/views/HomePage/Learn/index.js
@@ -5,8 +5,11 @@ import Button from '@/components/Button'
 import Title from '@/components/Title'
 
 import v60Img from '../../../../public/v60.jpg'
+import data from '@/data/HomePage/learnSection.json'
 
 function LearnSection() {
+  const { title, content, cta } = data
+
   return (
     <SectionWrapper
       id="learn"
@@ -16,23 +19,15 @@ function LearnSection() {
         <Image src={v60Img} alt="V60" height={400} width={400} />
         <div className="flex items-center justify-content flex-col w-[600px] mx-10">
           <Title element="h2" className="">
-            Learn
+            {title}
           </Title>
-          <p className="text-body my-8 text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <p className="text-body my-8 text-justify">{content}</p>
           <div className="flex items-start justify-start flex-row">
             <Button
               secondary="true"
               className="w-[400px] mx-4 hover:duration-1000"
             >
-              Brewing Guides
+              {cta}
             </Button>
           </div>
         </div>
